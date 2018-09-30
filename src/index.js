56de9ea93c4d21330abbe3b9429d39c581a99ab9
“Simaxa7” <simaxa7@mail.ru>
2018-09-30 23:39:50 +0300

    Review of a code. New design

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-	var arr = [];
-	arr2=bracketsConfig.length;
-	for (var i = 0; i < bracketsConfig.length; i++) {
-		arr[i]=bracketsConfig[i][0]+bracketsConfig[i][1];
+	
+	let openAndCloseBrakets = [];
+	
+	let strForChange = str;
+	let brackets = bracketsConfig;
+	let bracketsConfigLength = brackets.length;
+
+	for (let i = 0; i < bracketsConfigLength; i++) {
+		openAndCloseBrakets[i] = brackets[i][0] + brackets[i][1];
 	}
 
-	for (var i = 1; i < 2 && i>0 ; i++) {
-		let startLength=str.length;
+	for (let i = 1; i < 2; i++) {
+		let startLength = strForChange.length;
 			
-			for (var j = 0; j < arr.length; j++) {
-				str = str.split(arr[j]).join('');
-			}
-
-		let finishLength=str.length;
-		
-		i = startLength==finishLength ? 100 : 0;
-		if (finishLength == 0){
-			return true
+		for (let j = 0; j < openAndCloseBrakets.length; j++) {
+			strForChange = strForChange.split(openAndCloseBrakets[j]).join('');
 		}
+
+		let finishLength = strForChange.length;
+		
+		if(startLength !== finishLength) i = 0;
+		if (finishLength === 0) return true;
+
 	}
+
 	return false
 }
-
